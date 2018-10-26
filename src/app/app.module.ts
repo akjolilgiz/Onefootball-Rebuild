d9ffedcbf43d11686e89bb939de3aaaccea0ed5f
Guest User <Guest@epicodus-20.local>
2018-10-26 11:01:40 -0700

    Add popular pages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PopularPagesComponent } from './popular-pages/popular-pages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PopularPagesComponent
   ],
   imports: [
     BrowserModule
