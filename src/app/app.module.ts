dc9aa000737b36f12a5e55b36d307dd69daf6d0d
Guest User <Guest@epicodus-20.local>
2018-10-26 13:58:21 -0700

    add another component for top news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PopularPagesComponent } from './popular-pages/popular-pages.component';
+import { TopNewsComponent } from './top-news/top-news.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PopularPagesComponent
+    PopularPagesComponent,
+    TopNewsComponent
   ],
   imports: [
     BrowserModule
