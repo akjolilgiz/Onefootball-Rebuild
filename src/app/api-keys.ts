0c2075a6454f9ae38f4b79d1e01415e70f2c383b
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-11-10 08:30:17 -0800

    remove api-key from git ignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyCVe8PFsThy4-Urzdru6bt8W7UWwCfmehE",
+  authDomain: "onefootball-58ad1.firebaseapp.com",
+  databaseURL: "https://onefootball-58ad1.firebaseio.com",
+  projectId: "onefootball-58ad1",
+  storageBucket: "",
+  messagingSenderId: "556785995924"
+  };
